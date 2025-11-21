6b4ed61bb09fb19c76a627f43e9ca97e7b962549
AquilaXk <illusiveman7@gmail.com>
2025-11-21 14:47:12 +0900

    rename

--- src/main/java/com/back/BackApplication.java
+++ src/main/java/com/back/BackApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class P140971Application {
+public class BackApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(P140971Application.class, args);
+        SpringApplication.run(BackApplication.class, args);
     }
 
 }
