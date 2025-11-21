a0c6d7a1b1f0e48659e5e04db71eaa099c49727e
AquilaXk <illusiveman7@gmail.com>
2025-11-21 14:44:28 +0900

    work

---
+++ src/main/java/com/back/P140971Application.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P140971Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P140971Application.class, args);
+    }
+
+}
